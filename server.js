9fd552d7bcab0b21fb093657060b6828dff1e392
DanielFV0510 <farrovelawillinton@gmail.com>
2024-02-04 21:06:57 -0500

    cambio del puerto

--- server.js
+++ server.js
@@ -2,5 +2,5 @@ const express = require('express');
 
 const app = express();
 app.get('/', (req, res) => res.send('<h1>Hello world con express y HTML !!</h1>'))
-app.listen(3000);
-console.log('SERVER ON PORT 3000')
+app.listen(8080);
+console.log('SERVER ON PORT 80')
